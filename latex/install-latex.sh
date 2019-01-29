3cdeb6ef1fc7b23c4374e55c67cfd0a0bd70693c
Erik Lundmark <cjiqqq@gmail.com>
2019-01-29 15:11:49 +0100

    Update latex scripts

--- latex/install-latex.sh
+++ latex/install-latex.sh
@@ -7,7 +7,9 @@ sudo pacman -S texlive-most --noconfirm
 
 disp "Creating LaTex template folders.."
 mkdir -p ~/.latex/templates
-sudo cp texplate /usr/local/bin
+sudo cp scrips/texplate /usr/local/bin
+sudo cp scripts/vtex /usr/local/bin
 disp "Use ${Red}texplate list${Gre} to show available templates"
 disp "And use ${Red}texplate copy <template> <destination>${Gre} to copy named template to given folder"
+disp "Then open vim in LaTex mode via ${Red}vtex <name-of-latex-file>"
 disp "Done! Have a nice day."
